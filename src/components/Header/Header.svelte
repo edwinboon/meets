63a85180c9dfe84a7536b3a5a59d17f01b61df55
Edwin Boon <hello@edwinboon.dev>
2021-01-20 20:13:13 +0100

    Add Header and styles for it

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,28 @@
+<script></script>
+
+<style>
+    header {
+        align-items: center;
+        background: var(--color-primary);
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px;
+        display: flex;
+        height: 7.2rem;
+        justify-content: center;
+        left: 0;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+
+    .logo {
+        color: var(--color-white);
+        font-family: var(--font-family-title);
+        font-size: var(--font-size-title);
+        font-weigth: var(--font-weight-bold);
+        margin-left: 1.6rem;
+    }
+</style>
+
+<header>
+        <a href="/"><div class="logo">Meets <a/></div>
+</header>
