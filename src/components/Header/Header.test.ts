834cb6add3d4952cebbe32bb18e685fc66577d26
Edwin Boon <hello@edwinboon.dev>
2021-01-20 21:48:23 +0100

    Setup meets structure and adjust test

--- src/components/Header/Header.test.ts
+++ src/components/Header/Header.test.ts
@@ -1,13 +1,15 @@
 import Header from './Header.svelte';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 
 describe('Header', () => {
     test('it should render Header component', () => {
         // Arrange
-        const { getByText } = render(Header);
-        // Act
-        const header = getByText('Meets');
+         render(Header);
+
+         // Act
+        const header = screen.getByText('Meets');
+        
         // Assert
-        expect(Header).toBeTruthy();
+        expect(header).toBeTruthy();
     })
 })
